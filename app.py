6c50ed6c5f2609072f379bc4883ff6a6d65cfa7c
KubaSiwiec <jakubsiwiec.nwm@gmail.com>
2023-10-29 21:09:42 +0100

    Accept only valid entries. Create dashboard placeholder

--- app.py
+++ app.py
@@ -3,34 +3,45 @@ import streamlit as st
 # Create a Streamlit app
 st.title("Habit Tracker App")
 
-# Habit Name Input
-habit = st.text_input("Enter a habit:")
-
 # Create a list of days of the week
 days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-if habit:
-    # Habit Duration
-    duration = st.number_input("How long do you want to stick to this habit (in days)?", min_value=1)
+# Sidebar menu for navigation
+menu = st.sidebar.selectbox("Menu", ["Habit Entry", "Habit Dashboard"])
 
-    # Activity Time
-    st.write("Select the time for the activity:")
-    hours = st.slider("Hours", 0, 23)
-    minutes = st.slider("Minutes", 0, 59)
+if menu == "Habit Entry":
+    # Habit Entry View
+    st.header("Habit Entry")
 
-    # Days of the Week Selection
-    st.write("Select the days of the week for the habit:")
-    selected_days = [day for day in days_of_week if st.checkbox(day)]
+    # Habit Name Input
+    habit = st.text_input("Enter a habit:")
 
-    # Check if time of activity is zero or no days are selected
-    conditions_met = (hours == 0 and minutes == 0) or not selected_days
+    if habit:
+        # Habit Duration
+        duration = st.number_input("How long do you want to stick to this habit (in days)?", min_value=1)
 
-    if st.button("Save Habit"):
+        # Activity Time
+        st.write("Select the time for the activity:")
+        hours = st.slider("Hours", 0, 23)
+        minutes = st.slider("Minutes", 0, 59)
+
+        # Days of the Week Selection
+        st.write("Select the days of the week for the habit:")
+        selected_days = [day for day in days_of_week if st.checkbox(day)]
+
+        # Check if time of activity is zero or no days are selected
+        conditions_met = (hours == 0 and minutes == 0) or not selected_days
+
         if conditions_met:
             st.error("Please ensure you've selected at least one day and set a non-zero time for the activity.")
-        else:
+        elif st.button("Save Habit"):
             # Process and store the habit details
             st.write("Habit Name:", habit)
             st.write("Habit Duration (days):", duration)
             st.write("Activity Time:", f"{hours:02d}:{minutes:02d}")
             st.write("Frequency:", selected_days)
+
+elif menu == "Habit Dashboard":
+    # Habit Dashboard View
+    st.header("Habit Dashboard")
+    # Implement your habit dashboard functionality here
