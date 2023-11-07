42d4574f8e4c7c41a3f1501391aef4cb2bdddb77
KubaSiwiec <jakubsiwiec.nwm@gmail.com>
2023-11-07 21:38:58 +0100

    [feature] Add base for MongoService and HabitService

---
+++ services/mongo_service.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+import os
+
+class MongoService:
+    def __init__(self) -> None:
+        self.client = MongoClient(os.getenv("MONGO_CONNECTION_STRING"))
+        self.db = self.client.HabitTracker
+
+
+class HabitService(MongoService):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.habits_collection = self.db.habits
+
+    def insert_habit(self, habit: dict):
+        post_id = self.habits_collection.insert_one(habit).inserted_id
+        print(post_id)
