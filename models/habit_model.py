ec54861731c59562e197271cadf09818f14d5a20
KubaSiwiec <jakubsiwiec.nwm@gmail.com>
2023-11-07 21:50:49 +0100

    [feature] Add validation for HabitModel

--- models/habit_model.py
+++ models/habit_model.py
@@ -7,5 +7,14 @@ from models.weekday import Weekday
 class HabitModel:
     name: str
     number_of_days: int
-    time_per_day: int
+    daily_hours: int
+    daily_minutes: int
     weekdays: tuple[Weekday]
+
+    def __post_init__(self):
+        assert isinstance(self.number_of_days, str)
+        assert isinstance(self.daily_hours, int)
+        assert self.daily_hours in range(1, 25)
+        assert isinstance(self.daily_minutes, int)
+        assert self.daily_minutes in range(1, 61)
+        assert isinstance(self.weekdays, tuple[Weekday])
